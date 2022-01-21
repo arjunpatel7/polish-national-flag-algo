27570e2c7ffcc9f2ae23de7e39209f05827dae53
Arjun Patel <arjunkirtipatel@gmail.com>
2022-01-21 12:41:37 -0600

    Implemented bubble sort
    Implemented the simplest sort algo that came to mind, bubble sort! Also got printing working as well. woohoo!
    
    I want to implement other sorting algorithms, add a function that calculates statistics on the performance of each algorithm, and also come up with a strategy on my own.
    
    I have yet to find an appropriate visualization method as well. Not only do I want to visualize the games, but having a visualization of performance of each algo as they each do
    the tests could be cool.

--- polish-national-flag-algo/simulator.py
+++ polish-national-flag-algo/simulator.py
@@ -5,7 +5,7 @@ import random
 
 class PolishCheckers:
     # an instance of the polish national flag problem game.
-    # We'll represent red as 1 and black as 0
+    # We'll represent red as 0 and black as 1
 
 
     def __init__(self, num_checkers):
@@ -15,6 +15,11 @@ class PolishCheckers:
         self.history = []
         self.win = False
 
+    def inspect(self, c1, c2):
+        checker_1 = self.checkers[c1]
+        checker_2 = self.checkers[c2]
+        self.num_inspections += 2
+        return checker_1 < checker_2
     def swap(self, c1, c2):
         # swap the locations of two checkers
         # accepts index of each checker
@@ -35,12 +40,26 @@ class PolishCheckers:
 
     # quick sort
 
+    # bubble sort
+
+    def bubble_sort(self):
+        self.win_condition()
+        while not self.win:
+            # move along the entries until each pair is either swapped or not
+            for c_ind in range(0, len(self.checkers)-1):
+                pc.print_status()
+                if not self.inspect(c_ind, c_ind + 1):
+                    self.swap(c_ind, c_ind + 1)
+            self.win_condition()
+        pc.print_status()
+        print("Number of swaps: ", self.num_swaps)
+        print("Number of inspections: ", self.num_inspections)
+        return [self.num_swaps, self.num_inspections]
 
 
 
 
-
-pc = PolishCheckers(5)
-pc.print_status()
-pc.swap(0, 4)
-pc.print_status()
+for x in range(0, 5):
+    pc = PolishCheckers(15)
+    pc.print_status()
+    print(pc.bubble_sort())
